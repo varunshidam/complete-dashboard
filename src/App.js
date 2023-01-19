c6864dab0eb5110ba7901528d75a1c59b9d9acf5
Varun Sidam <varunshidam@gmail.com>
2023-01-19 15:32:52 +0530

    dashboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import { Dashboard } from "./scene/dashboard";
+import Contacts from "./scene/Contacts";
+import Bar from "./scene/bar";
+import Calendar from "./scene/calendar";
+import Dashboard from "./scene/dashboard";
+import FAQ from "./scene/faq";
+import Form from "./scene/form";
+import Geography from "./scene/geography";
 import Sidebar from "./scene/global/Sidebar";
 import Topbar from "./scene/global/Topbar";
+import Invoices from "./scene/invoices";
+import Line from "./scene/line";
+import Pie from "./scene/pie";
 import Team from "./scene/team";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
@@ -20,6 +29,15 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/geography" element={<Geography />} />
             </Routes>
           </main>
         </div>
